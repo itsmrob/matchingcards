e5b28a31b9f3278c98cb03469d75e4c8ebe6e769
Roberto <Robert_jhs@hotmail.com>
2024-02-07 22:34:10 -0600

    Validate cards matching implemented

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,5 +1,5 @@
+import React, { useState } from 'react'
 import { CardValues } from '../../types';
-import React from 'react'
 import Card from '../Card/Card';
 
 import './Board.css'
@@ -11,15 +11,63 @@ interface BoardProps {
 }
 
 const Board: React.FC<BoardProps> = ({ cards, onPlay }) => {
-    const handleClick = (cardId: number) => {
-        const newCards = cards.map((card) => {
-            if (card.id === cardId) {
-                return { ...card, flipped: !card.flipped }
+    //Si el parid de la primera imagen es igual que la segunda, mutar ambos objetos a true
+    //Si el pairId de la priemra es diferente, dejarlo como estaba y setear flipped false
+
+    const [selectedCards, setSelectedCards] = useState<CardValues[]>([]);
+
+
+
+    const validateCards = (card1: CardValues, card2: CardValues) => {
+        let updatedCards;
+        if (card1.pairId === card2.pairId) {
+            // Si las cartas hacen match, marcamos ambas como encontradas
+            updatedCards = cards.map(card => {
+                if (card.id === card1.id || card.id === card2.id) {
+                    return { ...card, found: true };
+                }
+                return card;
+            });
+        } else {
+            // Si no hacen match, volteamos ambas cartas de nuevo después de un breve retraso para permitir que el usuario vea la segunda carta
+            setTimeout(() => {
+                updatedCards = cards.map(card => {
+                    if (card.id === card1.id || card.id === card2.id) {
+                        return { ...card, flipped: false };
+                    }
+                    return card;
+                });
+                onPlay(updatedCards); // Actualizamos las cartas en la UI después del retraso
+            }, 1000);
+            return; // Salimos temprano para no ejecutar onPlay inmediatamente
+        }
+
+        // Actualizamos el estado de las cartas en la UI
+        onPlay(updatedCards);
+        // Reseteamos las cartas seleccionadas después de validar
+        setSelectedCards([]);
+    }
+
+    const handleClick = (selectedCard: CardValues) => {
+        const updatedCards = cards.map((card) => {
+            if (card.id === selectedCard.id) {
+                return { ...card, flipped: true };
             }
             return card;
         });
-        onPlay(newCards)
-    }
+        onPlay(updatedCards);
+
+        setSelectedCards(prevSelected => {
+            const newSelected = prevSelected.length < 2 ? [...prevSelected, selectedCard] : prevSelected;
+            if (newSelected.length === 2) {
+                validateCards(newSelected[0], newSelected[1]);
+            }
+            return newSelected;
+        });
+    };
+
+
+
     return (
         <div className='game-board' >
             {cards.map((card) => (
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,13 +5,14 @@ import lockedCard from '../../assets/ventana-de-chat.png';
 
 interface CardProps {
     card: CardValues;
-    onCardClick: (cardId: number) => void;
+    onCardClick: (cardId: CardValues) => void;
 }
 
 const Card: React.FC<CardProps> = ({ card, onCardClick }) => {
     return (
-        <div className='game-card' onClick={() => onCardClick(card.id)}>
-            <img src={card.flipped ? card.image : lockedCard} width={75} height={75} alt={card.id.toString()} />
+        <div className='game-card' onClick={() => onCardClick(card)}>
+            <img src={card.flipped || card.found ? card.image : lockedCard} width={75} height={75} alt={card.id.toString()} />
+            {/* <img src={card.image} width={75} height={75} alt={card.id.toString()} /> */}
         </div>
     )
 }
