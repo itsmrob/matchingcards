33d114c1c543b0cc02f4ae51ab1f4ff65239524c
Roberto <Robert_jhs@hotmail.com>
2024-02-07 20:00:19 -0600

    Cards were correctly aligned and styled

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -2,18 +2,22 @@ import React from 'react'
 import { CardValues } from '../../types';
 import Card from '../Card/Card';
 
+import './Board.css'
+
 
 interface BoardProps {
     cards: CardValues[],
     onPlay: (i: number) => void;
 }
 
-const Board: React.FC<BoardProps> = ({ cards }) => {
+const Board: React.FC<BoardProps> = ({ cards, onPlay }) => {
+    const handleClick = (cardId: number) => {
 
+    }
     return (
         <div className='game-board' >
             {cards.map((card) => (
-                <Card key={card.id}/>
+                <Card key={card.id} card={card} onCardClick={handleClick} />
             ))}
         </div>
     )
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './Card.css';
+import { CardValues } from '../../types';
 
-const Card = () => {
+interface CardProps {
+    card: CardValues;
+    onCardClick: (cardId: number) => void;
+}
+
+const Card: React.FC<CardProps> = ({ card, onCardClick }) => {
     return (
-        <div>Card</div>
+        <div className='game-card'>
+            <img src={card.image} width={75} height={75} alt={card.id.toString()} />
+        </div>
     )
 }
 
